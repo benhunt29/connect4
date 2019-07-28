5b95cabdcbbeefb5c7da6fcdb28dfc84f62f1928
benhunt29 <benhunt29@gmail.com>
2019-07-28 11:38:58 -0500

    fix some styling issues

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import Styled from "styled-components";
 
 import GameContainer from "./containers/GameContainer";
 
-const Wrapper = Styled.div`
-  height: 100vh;
-`;
-
 const GameContainerWrapper = Styled.div`
   padding-left: 20px;
   padding-right: 20px;
@@ -20,20 +16,22 @@ const Header = Styled.div`
 
 function App() {
   return (
-    <Wrapper className="columns is-mobile is-vcentered">
-      <div className="column">
-        <div className="columns is-centered is-mobile">
-          <div className="column is-full">
-            <Header className="title is-1">Connect4</Header>
+    <div className="container">
+      <div className="columns is-mobile is-vcentered">
+        <div className="column">
+          <div className="columns is-centered is-mobile">
+            <div className="column is-full">
+              <Header className="title is-1">Connect4</Header>
+            </div>
           </div>
+          <GameContainerWrapper className="columns is-mobile">
+            <div className="column">
+              <GameContainer />
+            </div>
+          </GameContainerWrapper>
         </div>
-        <GameContainerWrapper className="columns is-mobile">
-          <div className="column">
-            <GameContainer />
-          </div>
-        </GameContainerWrapper>
       </div>
-    </Wrapper>
+    </div>
   );
 }
 
